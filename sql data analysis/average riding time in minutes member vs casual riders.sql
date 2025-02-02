9f610b9a4b16cdcedc03de1448b326ee028c66f3
Amged_Alamari <165522771+EXECiiAA@users.noreply.github.com>
2025-02-01 20:22:35 -0500

    Delete sql data analysis directory

--- sql data analysis/average riding time in minutes member vs casual riders.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  member_casual,
-  ROUND(AVG(DATE_DIFF(ended_at, started_at, MINUTE)),2) AS average_riding_time_in_minutes
-
-FROM 
-  `tripdata-2023-d1.trip_data_2023.2023_combined_data`
-GROUP BY 
-  member_casual
-  
